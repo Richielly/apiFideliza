51e1e0ca996a8f91c995b8c11a0cb8b024dd470e
richielly <richielly.positivo@gmail.com>
2021-09-19 13:26:44 -0300

    V_0.0.2

--- venvapifideliza/main.py
+++ venvapifideliza/main.py
@@ -1,16 +1,51 @@
-# This is a sample Python script.
+from fastapi import FastAPI
+from pydantic import BaseModel
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = FastAPI()
 
+#rota raiz
+@app.get("/")
+def home():
+    return ("Todos Conectados!!! Bem vindo ao Fideliza !!!")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Usuario(BaseModel):
+    id: int
+    email: str
+    senha: str
 
+#base de dados para teste
+base_de_dados = [
+    Usuario(id=1, email='teste1@teste', senha='teste123'),
+    Usuario(id=2, email='teste2@teste', senha='teste234')
+]
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('Richielly')
+#rota para buscar todos os dados no banco
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+@app.get('/all_users')
+def all_users():
+    return base_de_dados
+
+#rota para buscar por id os dados no banco
+
+@app.get('/user')
+def user(id_user:int):
+    for user in base_de_dados:
+        if (user.id == id_user):
+            return user
+    return {"Status": 404, "Mensagem": "Usuario não encontrado."}
+
+@app.post('/user')
+def insert_user(user:Usuario):
+    #Regra de negocio com as restrições aqui
+
+    base_de_dados.append(user)
+    return user
+
+@app.delete('/delete')
+def delete_user(id_user:int):
+    for user in base_de_dados:
+        if(user.id == id_user):
+            base_de_dados.remove(user)
+            return {"Status": 200, "Mensagem": "Usuario deletado com sucesso."}
+
+    return {"Status": 404, "Mensagem": "Usuario não encontrado."}
